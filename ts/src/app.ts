1d9687d3a49ea5954dd5519e6d19a04a4ff77f9f
j-freddy <jtf.imperial@gmail.com>
2022-06-05 19:28:52 +0100

    Name app title in header, remove tests

--- ts/src/app.ts
+++ ts/src/app.ts
@@ -6,13 +6,5 @@ window.addEventListener("load", () => {
 
   const game = new Game();
   const gui = GUI.getInstance(game);
-
-  game.makeMove(3, 5);
-  game.makeMove(4, 6);
-  game.makeMove(5, 7);
-  game.makeMove(4, 7);
-  game.makeMove(4, 1);
-  game.makeMove(4, 8);
-
   gui.refresh();
 });
