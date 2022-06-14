af11d10b4786f0f972b84ecd4dd5988e2a629680
Freddy Jiang <jtf.imperial@gmail.com>
2022-06-14 12:04:26 +0100

    Make local board frame colour lighter

--- ts/src/gui/GUIData.ts
+++ ts/src/gui/GUIData.ts
@@ -1,6 +1,6 @@
 const GUIData = {
   activeColour: "#d8f5a2",
-  localFrameColour: "#868e96",
+  localFrameColour: "#adb5bd",
   globalFrameColour: "#212529",
   localFrameThickness: 1,
   globalFrameThickness: 4,
