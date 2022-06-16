a0d0d49c4a585006f23dfc798d07b44a7d69de5f
Freddy Jiang <jtf.imperial@gmail.com>
2022-06-16 19:51:08 +0100

    Refactor playerAIRandom methods to abstract class playerAI

---
+++ ts/src/model/player/playerAI.ts
@@ -0,0 +1,72 @@
+abstract class PlayerAI implements Player {
+  protected readonly markType: MarkType;
+  protected optimalMove: BoardPosition;
+  protected deprecated: boolean;
+
+  constructor(markType: MarkType) {
+    this.markType = markType;
+    // TODO Spaghetti
+    this.optimalMove = {
+      globalIndex: 0,
+      localIndex: 0
+    }
+    this.deprecated = false;
+  }
+
+  getMarkType(): MarkType {
+    return this.markType;
+  }
+
+  isBot(): boolean {
+    return true;
+  }
+
+  setDeprecated(): void {
+    this.deprecated = true;
+  }
+
+  // This is the ONLY method a new AI has to implement
+  // The AI must set its chosen move in this.optimalMove within the time frame
+  protected calculateOptimalMove(boardCopy: GlobalBoard): void {
+    throw new Error("calculateOptimalMove not implemented.");
+  }
+
+  chooseMove(boardCopy: GlobalBoard): Promise<BoardPosition> {
+    this.calculateOptimalMove(boardCopy);
+
+    return new Promise((resolve, reject) => {
+      // TODO Currently set AI think time to 500 seconds
+      setTimeout(() => {
+        if (this.deprecated) {
+          reject();
+        } else {
+          resolve(this.optimalMove);
+        }
+      }, 500);
+    });
+  }
+
+  // The rest of this class consists of useful methods that assist making an AI
+
+  protected getValidMoves(board: GlobalBoard): BoardPosition[] {
+    let validMoves: BoardPosition[] = [];
+
+    let activeBoards = board.getActiveBoards();
+
+    for (let localBoard of activeBoards) {
+      let cellValues = localBoard.getCellsValues();
+
+      for (let i = 0; i < cellValues.length; i++) {
+        // Cell is empty
+        if (!cellValues[i]) {
+          validMoves.push({
+            globalIndex: localBoard.getIndex(),
+            localIndex: i
+          });
+        }
+      }
+    }
+
+    return validMoves;
+  }
+}
