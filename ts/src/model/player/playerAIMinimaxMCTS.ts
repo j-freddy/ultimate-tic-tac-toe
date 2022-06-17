23d03c2993da1bba220d124547ed31093e6028f4
j-freddy <jtf.imperial@gmail.com>
2022-06-17 17:13:13 +0100

    Make function for AI to find all moves that win on a local board

--- ts/src/model/player/playerAIMinimaxMCTS.ts
+++ ts/src/model/player/playerAIMinimaxMCTS.ts
@@ -3,6 +3,31 @@ class PlayerAIMinimaxMCTS extends PlayerAI {
     super(markType);
   }
 
+  private markTypeToStatus(markType: MarkType) {
+    return markType === MarkType.O ?
+           BoardStatus.NoughtWin : BoardStatus.CrossWin;
+  }
+
+  // Get all moves that wins a local board
+  private getMovesThatWinsLocalBoard(board: GlobalBoard,
+                                     markType: MarkType): BoardPosition[] {
+    let winningMoves = [];
+
+    for (let move of this.getValidMoves(board)) {
+      // Copy board state and make move
+      let boardCopy = board.copy();
+      boardCopy.setCellValue(markType, move.globalIndex, move.localIndex);
+
+      // Check win
+      let localBoard = boardCopy.getLocalBoardByIndex(move.globalIndex);
+      if (localBoard.getStatus() === this.markTypeToStatus(markType)) {
+        winningMoves.push(move);
+      }
+    }
+
+    return winningMoves;
+  }
+
   // Get move that wins game immediately
   private getMoveThatWinsGame(board: GlobalBoard,
                               markType: MarkType): BoardPosition | null {
@@ -12,10 +37,7 @@ class PlayerAIMinimaxMCTS extends PlayerAI {
       boardCopy.setCellValue(markType, move.globalIndex, move.localIndex);
 
       // Check win
-      let statusWin = markType === MarkType.O ?
-                      BoardStatus.NoughtWin : BoardStatus.CrossWin;
-
-      if (boardCopy.getStatus() === statusWin) {
+      if (boardCopy.getStatus() === this.markTypeToStatus(markType)) {
         return move;
       }
     }
@@ -26,6 +48,23 @@ class PlayerAIMinimaxMCTS extends PlayerAI {
 
   protected calculateOptimalMove(boardCopy: GlobalBoard): void {
     let validMoves = this.getValidMoves(boardCopy);
-    this.optimalMove = validMoves[Math.floor(Math.random()*validMoves.length)];
+    let winningMove = this.getMoveThatWinsGame(boardCopy, this.markType);
+
+    // TODO Delete
+    // Choose winning move if exists
+    if (winningMove) {
+      this.optimalMove = winningMove;
+    } else {
+      // Otherwise choose move that wins a local board if exists
+      let goodMoves = this.getMovesThatWinsLocalBoard(boardCopy, this.markType);
+
+      if (goodMoves.length !== 0) {
+        this.optimalMove = goodMoves[0];
+      } else {
+        // Otherwise choose random move
+        this.optimalMove =
+          validMoves[Math.floor(Math.random()*validMoves.length)]; 
+      }
+    }
   }
 }
